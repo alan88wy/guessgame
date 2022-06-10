3cc01d8c9b5087519c813fb2af2e5c1af0dba23c
Alan <alan88w@yahoo.com>
2022-06-10 21:43:24 +0800

    added logic to check if it is a number

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,19 @@ fn main() {
             .expect("Failed to read line");
 
         // if guess.trim().to_lowercase() == "q" {
+
         if guess.trim().to_lowercase().eq("q") {
             println!("See ya!!");
             break;
         } else {
+            // match user_input
+            let guess: i64 = match guess.trim().parse::<i64>() {
+                Ok(inp) => inp,
+                Err(_) => {
+                    println!("Error, please enter a number!");
+                    continue;
+                }
+            };
             println!("You guessed: {}", guess);
         }
     }
